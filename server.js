a191f8da30d128c989b5864fa5a7f685d2829680
sammy wlodawski <samwlodawski@gmail.com>
2024-08-13 13:38:37 -0400

    Added session login authorization and created server file.

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-sessions');
+const exbhs = require('express-handlebars');
+const controllers = require('./controllers');
+const sequelize = require('sequelize');
+const SequelizeStore = require('connect-session-sequelize')
+(session.Store);
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const secret = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(secret));
+
+const hbs = exbhs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view-engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join( __dirname, 'public')));
+
+app.use(controllers);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => 
+        console.log(`Server Started on ${PORT}.`)
+    )
+});
