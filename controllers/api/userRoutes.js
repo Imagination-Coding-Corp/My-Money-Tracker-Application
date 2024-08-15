6f0eba1ddb2c7b7eac68d1ba31301ef47dbac186
sammy wlodawski <samwlodawski@gmail.com>
2024-08-15 00:56:10 -0400

    Changed route file names and created both add user and logged in routes.

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const {User, Transactions} = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { user_name:
+            req.body.user_name}});
+
+    if(!userData) {
+        res
+        .status(400)
+        .json({ message: 'You entered an incorrect username or password, check again'});
+        return;
+    }
+
+    req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.json({ user: userData, message: 'You are now logged in'});
+    });
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+
