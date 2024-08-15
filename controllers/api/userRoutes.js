e12dcc69d3ffcff87ead072dd54c01c6ecabf17f
sammy wlodawski <samwlodawski@gmail.com>
2024-08-15 14:35:15 -0400

    Created user routes file for get, post, delete.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -38,4 +38,36 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/logout', (req, res) => {
+    if(req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(200).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.get('/', async(req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        if(!userData) {
+            res.status(404).json({message: 'No user with this info exists'});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 
